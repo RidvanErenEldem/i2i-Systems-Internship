0382e52fc7f43de7da77a92733a262bee3f838fd
ridvanereneldem <erenvehbi18@gmail.com>
2021-07-28 15:55:47 +0300

    Added fix for second names

--- Homework-2/Question5.sql
+++ Homework-2/Question5.sql
@@ -8,10 +8,10 @@ select case
        || ' ' 
     else lpad(regexp_substr(customername, '[^ ]+' ,1,1),2) 
         || regexp_replace((substr(regexp_substr(customername, '[^ ]+' ,1,1),3,length(regexp_substr(customername, '[^ ]+' ,1,1)))) ,'.','*' )
-       || ' ' || 
-       lpad(regexp_substr(customername, '[^ ]+' ,1,2),2) 
-       || regexp_replace((substr(regexp_substr(customername, '[^ ]+' ,1,2),3,length(regexp_substr(customername, '[^ ]+' ,1,2)))) ,'.','*' )
+       || ' ' 
+       || regexp_replace((substr(regexp_substr(customername, '[^ ]+' ,1,2),1,length(regexp_substr(customername, '[^ ]+' ,1,2)))) ,'.','*' )
        || ' ' ||
        lpad(regexp_substr(customername, '[^ ]+' ,1,3),2) 
        || regexp_replace((substr(regexp_substr(customername, '[^ ]+' ,1,3),3,length(regexp_substr(customername, '[^ ]+' ,1,3)))) ,'.','*' )
     end as customername from HR.name 
+    
