0413b73c1b56cdaad2689fa6db7a02c58db880a1
ridvanereneldem <erenvehbi18@gmail.com>
2021-07-28 16:10:37 +0300

    Added fix for second name

--- Homework-2/Question5.sql
+++ Homework-2/Question5.sql
@@ -1,17 +1,17 @@
 select case 
     when regexp_count(HR.name.customername, ' ') = 1 then 
-        lpad(regexp_substr(customername, '[^ ]+' ,1,1),2) 
-        || regexp_replace((substr(regexp_substr(customername, '[^ ]+' ,1,1),3,length(regexp_substr(customername, '[^ ]+' ,1,1)))) ,'.','*' )
-       || ' ' || 
-       lpad(regexp_substr(customername, '[^ ]+' ,1,2),2) 
+          lpad(regexp_substr(customername, '[^ ]+' ,1,1),2) 
+       || regexp_replace((substr(regexp_substr(customername, '[^ ]+' ,1,1),3,length(regexp_substr(customername, '[^ ]+' ,1,1)))) ,'.','*' )
+       || ' '
+       || lpad(regexp_substr(customername, '[^ ]+' ,1,2),2) 
        || regexp_replace((substr(regexp_substr(customername, '[^ ]+' ,1,2),3,length(regexp_substr(customername, '[^ ]+' ,1,2)))) ,'.','*' )
        || ' ' 
     else lpad(regexp_substr(customername, '[^ ]+' ,1,1),2) 
-        || regexp_replace((substr(regexp_substr(customername, '[^ ]+' ,1,1),3,length(regexp_substr(customername, '[^ ]+' ,1,1)))) ,'.','*' )
+       || regexp_replace((substr(regexp_substr(customername, '[^ ]+' ,1,1),3,length(regexp_substr(customername, '[^ ]+' ,1,1)))) ,'.','*' )
        || ' ' 
        || regexp_replace((substr(regexp_substr(customername, '[^ ]+' ,1,2),1,length(regexp_substr(customername, '[^ ]+' ,1,2)))) ,'.','*' )
-       || ' ' ||
-       lpad(regexp_substr(customername, '[^ ]+' ,1,3),2) 
+       || ' '
+       || lpad(regexp_substr(customername, '[^ ]+' ,1,3),2) 
        || regexp_replace((substr(regexp_substr(customername, '[^ ]+' ,1,3),3,length(regexp_substr(customername, '[^ ]+' ,1,3)))) ,'.','*' )
     end as customername from HR.name 
-    
+
